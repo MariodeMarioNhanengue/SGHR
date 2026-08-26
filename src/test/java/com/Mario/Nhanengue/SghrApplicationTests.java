01ced28f5fc6c08258f30594bb07a7be500b09bd
Mario de Mario Nhanengue <mariopalmira7@gmail.com>
2026-08-26 17:41:51 +0200

    Estrutura do Projecto

---
+++ src/test/java/com/Mario/Nhanengue/SghrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mario.Nhanengue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SghrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
