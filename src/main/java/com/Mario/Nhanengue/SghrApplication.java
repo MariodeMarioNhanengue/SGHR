5372191e3895a3bd21bfc92dd7938f06bf2e5cf0
Mario de Mario Nhanengue <mariopalmira7@gmail.com>
2026-08-27 11:44:33 +0200

    Adiccao de arquivos html

---
+++ src/main/java/com/Mario/Nhanengue/SghrApplication.java
@@ -0,0 +1,13 @@
+package com.Mario.Nhanengue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SghrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SghrApplication.class, args);
+	}
+
+}
